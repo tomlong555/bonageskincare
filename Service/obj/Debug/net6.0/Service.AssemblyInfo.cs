5551ccdfdb171d06bfa7f8947bbab34d8f9ec38f
Tamrong chaiwiciht <gogogo1120@hotmail.com>
2024-01-04 22:14:39 +0700

    update

--- Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1887adb1d9aec5bbb364060f78de2eb29ccd8a40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
